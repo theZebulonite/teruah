368b39e9b64400a858ff075a0bce5375b36ef9c5
theZebulonite <click.albert.hover@gmail.com>
2025-02-03 15:39:05 -0600

    Añadir archivos pendientes

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,7 +2,9 @@
 const config = {
   plugins: {
     tailwindcss: {},
+    autoprefixer: {}, // Agrega autoprefixer para asegurarte de que el código CSS sea compatible con más navegadores.
   },
 };
 
 export default config;
+
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,9 +2,8 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",  // Asegúrate de que la ruta esté bien especificada
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",  // Si tienes componentes personalizados
   ],
   theme: {
     extend: {
