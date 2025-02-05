f91dd64110b09a7d7e3483383a392ef103bd7562
theZebulonite <click.albert.hover@gmail.com>
2025-02-04 19:42:39 -0600

    Configuración de GitHub Pages para Next.js

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",  // Habilita `next export`
+  basePath: "/teruah", // Cambia esto al nombre de tu repo en GitHub
+  images: {
+    unoptimized: true, // Necesario porque GitHub Pages no soporta optimización de imágenes Next.js
+  },
 };
 
 export default nextConfig;
