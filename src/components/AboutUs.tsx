91f7a2a78cdb18168425f7eefda6f0ac5466803f
theZebulonite <click.albert.hover@gmail.com>
2025-02-09 19:24:55 -0600

    Justificar texto de nosotros

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -18,7 +18,7 @@ const AboutUs = () => {
           {/* Texto */}
           <div>
             <h3 className="text-2xl font-semibold text-gray-900 mb-4">Acerca de nostroso.</h3>
-            <p className="text-gray-700 leading-relaxed">
+            <p className="text-gray-700 leading-relaxed text-justify">
             Para marcar la diferencia en el mundo no se necesitan grandes gestos, sino pequeños pasos de amor, valentía y fe. Una pequeña acción revolucionaria que puede cambiar la historia. La fuente de estos destellos de luz es nuestro Dios y Padre de las luces, que quiso depositar en Cristo toda su plenitud. Si estas unido a Él, estás completo, pleno y nada te falta. Así que sé generoso y da los dones que Él ha puesto en ti por Cristo y observa cómo el mundo cambia. No busques hacer el cambio desde afuera, ya que los cambios vienen desde dentro, así que siembra generosamente en tus generaciones amor, luz, fe, esperanza, valentía, amabilidad, paz, gratitud y verás crecer árboles de justicia. Nuestra intención es dar a las nuevas generaciones herramientas para sanar, disfrutar, crecer y gobernar. Únete al movimiento de la Verdad, la transformación y la expansión, y descubre las riquezas de Cristo.
             </p>
           </div>
