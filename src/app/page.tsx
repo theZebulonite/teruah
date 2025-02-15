2ba68e948056cb753d0cccb9ba56b827f331b198
theZebulonite <click.albert.hover@gmail.com>
2025-02-14 18:42:13 -0600

    revision de workflow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Header from "@/components/Header";
 import BooksSection from "@/components/BooksSection"; // Ajusta la ruta según sea necesario
-import Footer from "@/components/Footer"; // Ajusta la ruta según sea necesario
+import Footer from "@/components/Footer"; // Ajusta la ruta según sea necesario.
 
 
 export default function Home() {
